4c7c44a0249e16ec62c705d21dcc70ff675e0c32
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-06-03 23:05:42 -0500

    rows and columns

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,15 @@
 // API KEY: 0ha2JYIPcQ2JlisGgiDTdoH9mt6qXvdw
 
-//javascript, jQuery API
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0ha2JYIPcQ2JlisGgiDTdoH9mt6qXvdw&limit=10");
-xhr.done(function (response) { console.log("success got data", response); });
+//javascript, jQuery API -- from GIPHY Website
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0ha2JYIPcQ2JlisGgiDTdoH9mt6qXvdw&limit=10");
+// xhr.done(function (response) { console.log("success got data", response); });
+
+//javascript, jQuery API -- Based on Class Activity
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0ha2JYIPcQ2JlisGgiDTdoH9mt6qXvdw&limit=10";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log("response: ", response);
+});
