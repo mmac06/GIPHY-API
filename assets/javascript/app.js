4b2c1f1110faac7d9c4481ad95bd9e8c16d2d5ad
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-06-04 23:37:49 -0500

    added 'enter button' for search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,9 @@
 // xhr.done(function (response) { console.log("success got data", response); });
 
 // array for pre-loaded terms
-var startGIF = ["simpsons", "game of thrones", "family guy", "ice cube"];
+var gifArray = ["simpsons", "game of thrones", "family guy", "ice cube"];
+
+
 
 // function to get gifs to display
 function displayGIF() {
@@ -33,8 +35,6 @@ function displayGIF() {
 }
 
 
-
-
 // Function for displaying gifs data
 function renderButtons() {
 
@@ -42,23 +42,46 @@ function renderButtons() {
     // (this is necessary otherwise you will have repeat buttons)
     $("#terms").empty();
     // Loops through the array of gifs
-    for (var i = 0; i < startGIF.length; i++) {
+    for (var i = 0; i < gifArray.length; i++) {
 
         // Then dynamicaly generates buttons for each gif in the array
         var a = $("<button>");
         // Adds a class of gif to our button
         a.addClass("gif-button");
         // Added a data-attribute
-        a.attr("data-name", startGIF[i]);
+        a.attr("data-name", gifArray[i]);
         // Provided the initial button text
-        a.text(startGIF[i]);
+        a.text(gifArray[i]);
         // Added the button to the terms div
         $("#terms").append(a);
+    };
+};
+
+// This function handles events where the add add gif is clicked
+$("#searchGifs").on("click", function (e) {
+    e.preventDefault();
+    // This line of code will grab the input from the textbox
+    var gifAdd = $("#searchText").val().trim();
+
+    // The gif from the textbox is then added to the array
+    gifArray.push(gifAdd);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
+
+// function enables the user to press 'enter' in addition to clicking the search button
+$("#searchText").keyup(function (event) {
+    if (event.which == 13) {
+        $("#searchGifs").click();
     }
-}
+});
+
+
 
 // Adding click event listeners to all elements with a class of "gif-button"
 $(document).on("click", ".gif-button", displayGIF);
 
+// calls our functions on page load
 renderButtons();
 displayGIF();
