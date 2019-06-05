808e1027ef25b1df642651cbf18795d1c684b1cb
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-06-04 22:32:54 -0500

    added search term buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,9 @@
 // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0ha2JYIPcQ2JlisGgiDTdoH9mt6qXvdw&limit=10");
 // xhr.done(function (response) { console.log("success got data", response); });
 
+// array for pre-loaded terms
+var startGIF = ["simpsons", "game of thrones", "family guy", "ice cube"];
+
 //javascript, jQuery API -- Based on Class Activity
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0ha2JYIPcQ2JlisGgiDTdoH9mt6qXvdw&limit=10";
 
@@ -23,4 +26,27 @@ $.ajax({
 });
 
 
-// gifs.response.data.images.fixed_width.url
+// Function for displaying gifs data
+function renderButtons() {
+
+    // Deletes the gifs prior to adding new gif
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#terms").empty();
+    // Loops through the array of gifs
+    for (var i = 0; i < startGIF.length; i++) {
+
+        // Then dynamicaly generates buttons for each gif in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adds a class of gif to our button
+        a.addClass("gif");
+        // Added a data-attribute
+        a.attr("data-name", startGIF[i]);
+        // Provided the initial button text
+        a.text(startGIF[i]);
+        // Added the button to the buttons-view div
+        $("#terms").append(a);
+    }
+}
+
+renderButtons();
